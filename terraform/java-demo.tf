4971f68c0ba25b90a7082ad27d382927f4ebb941
Marcelo Oliveira <marcelo@marceloweb.info>
2018-08-07 16:22:14 -0300

    - new files

--- terraform/java-demo.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "java-demo" {
-  ami = "ami-66506c1c"
-  instance_type = "t2.micro"
-
-  provisioner "remote-exec" {
-     script = "puppet.sh"
-  }
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,54 @@
 provider "aws" {
-   region = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  
+  ami = "ami-66506c1c"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y &
+              apt-get install ruby -y &
+              gem install --no-document puppet &
+              apt-get install wildfly -y &
+              service wildfly start &
+              EOF
+
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  # Allow all outbound
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
