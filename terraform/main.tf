23b50abc6fceb9334f45799e1508ed6487ad0059
Marcelo Oliveira <marcelo@marceloweb.info>
2018-08-09 16:55:47 -0300

    - vagrant

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   
   ami = "ami-66506c1c"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = ["${aws_security_group.instance.id}",, "${aws_security_group.ssh.id}", "${aws_security_group.all_outgoing.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -33,22 +33,48 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_security_group" "elb" {
-  name = "terraform-example-elb"
+resource "aws_iam_user" "user" {
+  name = "test-user"
+  path = "/"
+}
 
-  # Allow all outbound
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_iam_user_ssh_key" "user" {
+  username   = "${aws_iam_user.user.name}"
+  encoding   = "SSH"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 mytest@mydomain.com"
+}
 
-  # Inbound HTTP from anywhere
+resource "aws_security_group" "ssh" {
+  name = "ssh"
+  description = "Allow all ssh traffic"
+  vpc_id = ""
+
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags {
+    Name = "ssh"
+  }
+}
+
+resource "aws_security_group" "all_outgoing" {
+  name = "all_outgoing"
+  description = "Allow all outgoing traffic"
+  vpc_id = ""
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags {
+    Name = "all_outgoing"
+  }
+
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,3 +2,11 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default = 8080
 }
+
+variable "wildfly-version" {
+	default = "9.0.1.Final"
+}
+
+variable "url" {
+	default = "https://download.jboss.org/wildfly/9.0.1.Final/wildfly-9.0.1.Final.zip"
+}
