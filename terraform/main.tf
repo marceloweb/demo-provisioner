8977f891dd2ca784b08871225d64efac78cce716
Marcelo Oliveira <marcelo@marceloweb.info>
2018-10-03 10:42:42 -0300

    - added ssh keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,4 +7,25 @@ resource "digitalocean_droplet" "java-demo" {
   name   = "web-1"
   region = "nyc1"
   size   = "s-1vcpu-1gb"
+  ssh_keys = ["fingerprint"]
+
+  connection {
+    user = "root"
+    type = "ssh"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    agent = false
+  }
+
+  provisioner "remote-exec" {
+     inline = [
+       "#!/bin/sh",
+       "apt-get update -y",
+       "apt-get install openjdk-8-jdk -y",
+       "cd /tmp",
+       "wget http://apt.puppet.com/puppetlabs-release-pc1-xenial.deb",
+       "dpkg -i puppetlabs-release-pc1-xenial.deb",
+       "apt-get -y install puppet-common"
+     ]
+  }
 }
+
